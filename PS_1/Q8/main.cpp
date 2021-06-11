fc06058dd85d66358d48f35e099f963247da11f8
LucasKIJ <lucas.jevtic@gmail.com>
2021-06-11 16:29:18 +0100

    Finished Q8

---
+++ PS_1/Q8/main.cpp
@@ -0,0 +1,113 @@
+  
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <cassert>
+#define size 2
+
+double vec_mult(double* x1, double* x2){
+    // Vector dot product function
+    double product = 0;
+    for (int i = 0; i < size; i++){
+        product = product + x1[i] * x2[i];
+    }
+    return product;
+}
+
+double* get_row(double** A, int i, int j){
+    double* row;
+    row = new double [size];
+    for (int j = 0; j < size; j++){
+        row[j] = A[i][j];
+    }
+    return row;
+}
+
+double* get_col(double** A, int i, int j){
+    double* col;
+    col = new double [size];
+    for (int i = 0; i < size; i++){
+        col[i] = A[i][j];
+    }
+    return col;
+}
+
+
+double** mat_mult(double** A, double** B){
+    double* temp1;
+    double* temp2;
+    double** prod;
+    prod = new double* [size];
+    for (int i = 0; i < size; i++){
+        prod[i] = new double [size];
+    }
+
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            
+            temp1 = get_row(A,i,j);
+            temp2 = get_col(B,i,j);
+            prod[i][j] = vec_mult(temp1,temp2);
+        }
+    }
+    return prod;
+}
+
+
+void print_mat(double** A){
+
+    for(int i=0; i < size; i++)
+    {
+        for(int j=0; j < size; j++)
+        {
+            std::cout << A[i][j]<<" ";
+        }
+        std::cout << "\n";
+    }
+    std:: cout <<"\n";
+}
+
+// *********** Main *********** //
+
+int main(){
+    // Define variables
+    double** A;
+    double** B;
+
+    A = new double* [size];
+    B = new double* [size];
+    for (int i = 0; i < size; i++){
+        A[i] = new double [size];
+        B[i] = new double [size];
+    }
+
+    std::cout << "Enter the first matrix (size " << size << "x" << size << ") \n";
+    // Input first matrix
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+             std::cout << "Enter the enter position [" << i << "," << j <<  "] \n";
+             std::cin >> A[i][j];
+        }
+    }
+
+    std::cout << "Enter the second matrix (size " << size << "x" << size << ") \n";
+    // Input second matrix
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+             std::cout << "Enter the enter position [" << i << "," << j <<  "] \n";
+             std::cin >> B[i][j];
+        }
+    }
+    
+    std::cout << "The first array you entered is: \n";
+    print_mat(A);
+
+    std::cout << "The second array you entered is: \n";
+    print_mat(B);
+
+    double** prod;
+    prod = mat_mult(A,B);
+    std::cout << "\n he matrix product is: \n";
+    print_mat(prod);
+}
